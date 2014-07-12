f52e80ae7381af44cbce1875ccce0e13f72537ea
Olav Vatne <olavvatne@gmail.com>
2014-07-12 13:27:21 +0200

    Height of cropped image is calculated by aspect ratio of width and circa apparatus height

--- src/analyze/ImageValueFinder.java
+++ src/analyze/ImageValueFinder.java
@@ -13,12 +13,27 @@ public class ImageValueFinder  {
 	
 	public static final int OG_END = 0;
 	public static final int OW_END = 2;
+	public static final int OW_HIGH = 3;
+	public static final int OW_LOW = 2;
+	public static final int OG_HIGH = 0;
+	public static final int OG_LOW = 1;
 	
+	//Photoshop measured ratio. TODO: Settings panel
+	public static double widthHeightRatio = 0.16;
+	
+	/**
+	 * GetValues opens the picture, and crop out the area of the picture between the markers.
+	 * FindValue is run for each cropped picture area. The result is values that indicate where
+	 * the seperation of oil/gas oil/water is... Basically clear and black.
+	 * @param name picture address
+	 * @param m list markers in the picture.
+	 * @return A list of measurements of where the seperation of dark and light is.
+	 */
 	public static List<Double> getValues(String name, List<ImageMarkerPoint> m) {
 		ImagePlus imp = new Opener().openImage(name);
 		ImagePlus imp2 = imp.duplicate();
-		imp = crop(imp, m.get(OG_END), m.get(1));
-		imp2 = crop(imp2, m.get(OW_END), m.get(3));
+		imp = crop(imp, m.get(OG_HIGH), m.get(OG_LOW));
+		imp2 = crop(imp2, m.get(OW_LOW), m.get(OG_HIGH));
 		
 		List<Double> values = new ArrayList<Double>();
 		values.add(findValue(imp, true, 100)+ m.get(OG_END).getX());
@@ -28,14 +43,26 @@ public class ImageValueFinder  {
 		return values;
 	}
 	
-	
+	/**
+	 * The findValue method assume that the image it gets is cropped and contain only the measurement area.
+	 * I.E just the glass part of the image is present, and all rows is available to me tested.
+	 * A number of rows will be tested, and the number is decided by sampleSize. After sampling, all
+	 * sample values is sorted. From the sorted 
+	 * @param imp Cropped image, of the surface to be measured
+	 * @param left The direction the samples should be taken in. Left = true, Right = false
+	 * @param sampleSize How many row should be sampled from the image surface.
+	 * @return Return the median.
+	 */
 	public static double findValue(ImagePlus imp, boolean left, int sampleSize) {
 		IJ.run(imp, "8-bit", "");
 		IJ.run(imp, "Smooth", "");
 		IJ.run(imp, "Smooth", "");
 		IJ.run(imp, "Make Binary", "");
 		
+		
 		int[] samples = new int[sampleSize];
+		
+		int outlierSize = (int)(sampleSize*0.10);
 		int yStart = (imp.getHeight()/2)-50;
 		//Må nok forbedre denne metoden, se kommenterte kode under.
 		for (int i = 0; i<sampleSize; i++) {
@@ -55,16 +82,23 @@ public class ImageValueFinder  {
 		return (( samples[sampleSize/2] + samples[(sampleSize/2)-1] ) / 2);
 	}
 	
-	
+	/**
+	 * Crop will crop a image after p1 and p2. The height of the image is decided by an width height ratio that probably wont change often.
+	 * The apparatus is often similar. Can be a value changed in the settings, or set in a calibrate panel. 
+	 * @param img The image to be cropped.
+	 * @param p1 The point where the image starts from the left.
+	 * @param p2 Point where image should be cropped to the right.
+	 * @return A cropped image.
+	 */
 	public static ImagePlus crop(ImagePlus img, ImageMarkerPoint p1,ImageMarkerPoint p2 ) {
 		int x = (int)p1.getX();
-		int y = (int) p1.getY() -200; //må vite mål ut ifra en jpanel i starten. CalibratePanel eller noe.
-		int w = (int) p2.getX()-(int)p1.getX();
-		int h = 400;
+		int x2 = (int) p2.getX();
+		int w = x2-x;
+		int h = (int) (w*widthHeightRatio); //height of apparatus //h can change so must have a setting or something
+		int y = (int) p1.getY() -(h/2); 
 		
 		img.setRoi(new Rectangle(x,y,w,h ));
 		IJ.run(img, "Crop", "");
-		
 		return img;
 	}
 	
