68bb11625acb21da0912f487a4158b99ef29b015
Olav Vatne <olavvatne@gmail.com>
2014-07-14 20:59:48 +0200

    Popup menu for rows in table. All rows in image table have two options. Open containing folder or open image (external program)

---
+++ src/view/mainPanel/ImagePopupMenu.java
@@ -0,0 +1,76 @@
+package view.mainPanel;
+
+import java.awt.Component;
+import java.awt.Desktop;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.JTable;
+
+import model.ImageTableModel;
+
+/**
+ * ImagePopupMenu extends JPopupMenu. Specialized popupmenu containing
+ * two options. Open containing folder or open image. When initing object, actionListeners are added
+ * to all menu items.
+ * 
+ * 	- Open containing folder - Open a finder/explorer window with the the content of the folder the image belongs to
+ *  - Open image - Open the image currently selected in an external program, like window image viewer.
+ * @author Olav
+ *
+ */
+public class ImagePopupMenu extends JPopupMenu {
+	private ImageTableModel model;
+	
+	public ImagePopupMenu(ImageTableModel tableModel) {
+		this.model = tableModel;
+		JMenuItem openFolder = new JMenuItem("Open containing folder");
+		openFolder.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				  	Component c = (Component)e.getSource();
+			        JPopupMenu popup = (JPopupMenu)c.getParent();
+			        JTable table = (JTable)popup.getInvoker();
+					int row = table.getSelectedRow();
+					displayFile(model.getDataModel(row).getFilePath(), true);
+				}
+				
+			
+		});
+		
+		JMenuItem inspectItem = new JMenuItem("Open image");
+		inspectItem.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					Component c = (Component)e.getSource();
+			        JPopupMenu popup = (JPopupMenu)c.getParent();
+			        JTable table = (JTable)popup.getInvoker();
+					int row = table.getSelectedRow();
+					displayFile(model.getDataModel(row).getFilePath(), false);
+				}
+				
+			
+		});
+		this.add(openFolder);
+		this.add(inspectItem);
+	}
+	
+	public void displayFile(String path, boolean parent) {
+		File dir = new File(path);
+		if(parent) {
+			dir = dir.getParentFile();
+		}
+		try {
+			Desktop.getDesktop().open(dir);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+}
--- src/view/mainPanel/ImageTablePanel.java
+++ src/view/mainPanel/ImageTablePanel.java
@@ -3,7 +3,6 @@ package view.mainPanel;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Insets;
-import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -18,7 +17,6 @@ import javax.swing.border.EmptyBorder;
 
 import automeasurer.Measurer;
 import view.imageViewer.MeasurementsPanel;
-import view.imageViewer.Viewer;
 import model.ImageDataModel;
 import model.ImageTableModel;
 /**
@@ -31,6 +29,7 @@ public class ImageTablePanel extends JPanel implements MouseListener, PropertyCh
 	private ImageTableModel model;
 	private JTable imageTable;
 	private JScrollPane listScroll;
+	private ImagePopupMenu popup;
 	
 	public ImageTablePanel(ImageTableModel model) {
 		this.setLayout(new BorderLayout());
@@ -44,29 +43,35 @@ public class ImageTablePanel extends JPanel implements MouseListener, PropertyCh
 		imageTable.setDefaultRenderer(Enum.class, new ColorRenderer(true));
 		listScroll.setViewportView(imageTable);
 		this.add(listScroll, BorderLayout.CENTER);
+		
+		popup = new ImagePopupMenu(this.model);
 		this.setBorder(new EmptyBorder(new Insets(10, 10, 10, 10)));
 	}
-
-
+	
+	
+	
+	
+	
+	private void openMeasurementPanel(MouseEvent e) {
+		int rowInTable = imageTable.getSelectedRow();
+		int row = imageTable.convertRowIndexToModel(rowInTable);
+		//pass på memory leak?? Sjekk det ut senere
+		ImageDataModel data = this.model.getDataModel(row);
+		JFrame frame = new JFrame();
+		MeasurementsPanel panel = new MeasurementsPanel(frame, data, model, row);
+		panel.addChangeListener(this);
+		
+		Dimension sz = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setMinimumSize(new Dimension((int)(sz.getWidth()/2), (int)(sz.getHeight()/2)));
+		frame.setPreferredSize(sz);// må vekk for at minimering skal faktisk minimere, maximize forårsaker layout krøll med initflytting
+		frame.pack();
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	}
 	public void mousePressed(MouseEvent e) {
 		if (e.getClickCount() == 2) {
-			Point p = e.getPoint();
-			int rowInTable = imageTable.getSelectedRow();
-			int row = imageTable.convertRowIndexToModel(rowInTable);
-			//pass på memory leak?? Sjekk det ut senere
-			ImageDataModel data = this.model.getDataModel(row);
-			JFrame frame = new JFrame();
-			MeasurementsPanel panel = new MeasurementsPanel(frame, data, model, row);
-			panel.addChangeListener(this);
-			
-			Dimension sz = Toolkit.getDefaultToolkit().getScreenSize();
-			frame.setMinimumSize(new Dimension((int)(sz.getWidth()/2), (int)(sz.getHeight()/2)));
-			frame.setPreferredSize(sz);// må vekk for at minimering skal faktisk minimere, maximize forårsaker layout krøll med initflytting
-			frame.pack();
-			frame.add(panel);
-			frame.setVisible(true);
-			frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			System.out.println(data.getFilePath());
+			openMeasurementPanel(e);
 		}
 
 	}
@@ -95,8 +100,19 @@ public class ImageTablePanel extends JPanel implements MouseListener, PropertyCh
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		if (e.isPopupTrigger())
+        {
+            JTable source = (JTable)e.getSource();
+            if(source instanceof JTable) {
+            	int row = source.rowAtPoint( e.getPoint() );
+                int column = source.columnAtPoint( e.getPoint() );
+
+                if (! source.isRowSelected(row))
+                    source.changeSelection(row, column, false, false);
+
+                popup.show(e.getComponent(), e.getX(), e.getY());
+            }
+        }
 	}
 
 
