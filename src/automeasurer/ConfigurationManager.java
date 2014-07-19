69578a8a879d8a1081d0b079222558566902ad3b
Olav Vatne <olavvatne@gmail.com>
2014-07-19 20:19:36 +0200

    Reset button for marker color

--- src/automeasurer/ConfigurationManager.java
+++ src/automeasurer/ConfigurationManager.java
@@ -12,58 +12,34 @@ public class ConfigurationManager {
 		return new ConfigurationManager();
 	}
 	
-	private Preferences config = null;
+	private Preferences config = Preferences.userRoot().userNodeForPackage(ConfigurationManager.class);;
 	
 	public void put(Setting key, double value) {
-		if(config == null) {
-			loadPreference();
-		}
 		config.putDouble(key.name(), value);
 	}
 	
 	public void put(Setting key, int value) {
-		if(config == null) {
-			loadPreference();
-		}
 		config.putInt(key.name(), value);
 	}
 	
 	public void put(Setting key, String value) {
-		if(config == null) {
-			loadPreference();
-		}
 		config.put(key.name(), value);
 	}
 	
 	public double getDouble(Setting key) {
-		if(config == null) {
-			loadPreference();
-		}
 		return config.getDouble(key.name(), key.defaultDouble());
 	}
 	
 	public int getInt(Setting key) {
-		if(config == null) {
-			loadPreference();
-		}
 		return config.getInt(key.name(), key.defaultInt());
 	}
 	
 	public String get(Setting key) {
-		if(config == null) {
-			loadPreference();
-		}
 		return config.get(key.name(), key.defaultString());
 	}
 	
 	public void reset(Setting key) {
-		if(config==null) {
-			loadPreference();
-		}
-		
+		config.remove(key.name());
 	}
 	
-	private void loadPreference() {
-		config = Preferences.userRoot().userNodeForPackage(ConfigurationManager.class);
-	}
 }
--- src/view/settingsPanel/AnalyzerSettings.java
+++ src/view/settingsPanel/AnalyzerSettings.java
@@ -1,7 +1,10 @@
 package view.settingsPanel;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
@@ -11,7 +14,7 @@ import javax.swing.event.ChangeListener;
 import automeasurer.ConfigurationManager;
 
 
-public class AnalyzerSettings extends BaseSettings implements ChangeListener {
+public class AnalyzerSettings extends BaseSettings implements ChangeListener, ActionListener {
 	private JSlider[] colorPicker1 = new JSlider[3];
 	private JSlider[] colorPicker2 = new JSlider[3];
 	private Setting[] colorConfig1 = new Setting[]{Setting.MIN_HUE,
@@ -68,6 +71,10 @@ public class AnalyzerSettings extends BaseSettings implements ChangeListener {
 		maxColor.setSize(40,40);
 		setColor(maxColor, colorPicker2);
 		this.add(maxColor);
+		
+		JButton resetButton = new JButton("Reset");
+		resetButton.addActionListener(this);
+		this.add(resetButton);
 	}
 	
 	private void setColor(JPanel label, JSlider[] hsb) {
@@ -96,5 +103,18 @@ public class AnalyzerSettings extends BaseSettings implements ChangeListener {
 	public void stateChanged(ChangeEvent e) {
 		setColor(minColor, colorPicker1);
 		setColor(maxColor, colorPicker2);
+	}
+
+	private void resetColorPicker(Setting[] hsb, JSlider[] picker) {
+		for(int i = 0; i<hsb.length; i++) {
+			config.reset(hsb[i]);
+			picker[i].setValue(config.getInt(hsb[i]));
+		}
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		resetColorPicker(colorConfig1, colorPicker1);
+		resetColorPicker(colorConfig2, colorPicker2);
+
 	}
 }
